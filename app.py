e16ce060df29b68f3f13f62296c7219afdb7ded1
YZL824 <y2268719438@email.swu.edu.cn.com>
2025-06-22 00:02:13 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+import random
+
+app = Flask(__name__)
+
+# 礼物列表
+gifts = ["香水", "手表", "书籍《活着为了什么》", "一次浪漫旅行", "手写情书", "定制首饰"]
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/draw")
+def draw():
+    gift = random.choice(gifts)
+    return jsonify({"gift": gift})
+
+if __name__ == "__main__":
+    app.run(debug=True)
